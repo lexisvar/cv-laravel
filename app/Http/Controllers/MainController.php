37353b54c94cf82873c1f5cddf623780c0fdb928
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-01-08 21:42:39 -0500

    Experience section

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Skill;
+use App\Models\Experience;
 
 class MainController extends Controller
 {
   public function index()
   {
-    $data['skills'] = Skill::all();  
+    $data['skills'] = Skill::all();
+    $data['experiences'] = Experience::all();  
+  
     return view('home', $data);
   }
 }
